9bdbcabe39db62e69f08a94bd76628a090fb6cfc
sagarrawandale07 <sagarrawandale07@gmail.com>
2024-06-10 15:23:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Weather from './components/Weather'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    
+    <div className='footer'>
+        
+    <div className="info">
+    <p>Designed By: Sagar Ravandale</p>
+    <p>sagarravandale07@gmail.com</p>
+    </div>
+      
+    </div>
+  )
+}
+
+export default Footer
